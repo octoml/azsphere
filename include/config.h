2c546e533e02d4b1017a51ef754993674ac38cb5
Mehrdad Hessar <mehrdad.hessar@gmail.com>
2020-06-09 18:25:33 -0600

    changed defines

--- include/config.h
+++ include/config.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "Hardware/mt3620_rdb/inc/hw/mt3620_rdb.h"
 
-// #define AS_DEBUG            0       //Enable debugging
-// #define AS_NETWORKING       0       //Enable networking
-// #define AS_ETHERNET_DEBUG   0       //Enable debugging messages over network
+// #define AS_DEBUG            1       //Enable debugging
+// #define AS_NETWORKING       1       //Enable networking
+// #define AS_NETWORK_DEBUG    0       //Enable debugging messages over network
 
 static int LED1[] = {MT3620_RDB_LED1_RED, MT3620_RDB_LED1_GREEN, MT3620_RDB_LED1_BLUE};
 static int LED2[] = {MT3620_RDB_LED2_RED, MT3620_RDB_LED2_GREEN, MT3620_RDB_LED2_BLUE};
