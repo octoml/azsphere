2fc0dc5fe884e8da31b8e35c294bfdc2f6b42e1d
Mehrdad Hessar <mehrdad.hessar@gmail.com>
2020-06-16 10:39:47 -0600

    fix branch dependency

--- include/runtime.h
+++ include/runtime.h
@@ -1,22 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 /* Explicitly declare posix_memalign function */
 #if _POSIX_C_SOURCE < 200112L
 #undef _POSIX_C_SOURCE
@@ -44,9 +25,9 @@
 /*! Maximum output entries in a GraphRuntime */
 #define GRAPH_RUNTIME_MAX_OUTPUTS           1               //default 300
 
-#define GRAPH_RUNTIME_NODE_NAME_MAX         5               //default 80
-#define GRAPH_RUNTIME_FUNC_NAME_MAX         110             //default 120
-#define GRAPH_RUNTIME_CTRL_DEPTH_SIZE       1               //default 200
+// #define GRAPH_RUNTIME_NODE_NAME_MAX         5               //default 80
+// #define GRAPH_RUNTIME_FUNC_NAME_MAX         110             //default 120
+// #define GRAPH_RUNTIME_CTRL_DEPTH_SIZE       1               //default 200
 
 #include <runtime/crt/crt_runtime_api.c>
 #include <runtime/crt/crt_backend_api.c>
